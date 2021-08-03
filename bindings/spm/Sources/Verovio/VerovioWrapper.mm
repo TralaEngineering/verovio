5a165cc5d94cd2df00a10bed17bcca5bd904f4aa
Josh Elkins <josh@trala.com>
2021-08-03 12:06:37 -0500

    Moved Verovio resource setup to same thread as load & render

--- bindings/spm/Sources/Verovio/VerovioWrapper.mm
+++ bindings/spm/Sources/Verovio/VerovioWrapper.mm
@@ -8,29 +8,17 @@
 
 static dispatch_queue_t renderQueue = dispatch_queue_create("com.rism.Verovio.sheetMusicRender", DISPATCH_QUEUE_SERIAL);
 
-#pragma mark - init & dealloc
-
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        static dispatch_once_t onceToken;
-        dispatch_once(&onceToken, ^{
-            NSBundle * verovioBundle = [BundleHelper bundleForModule];
-            NSString * resourcePath = [verovioBundle URLsForResourcesWithExtension:@"xml" subdirectory:@"data"].firstObject.URLByDeletingLastPathComponent.path;
-            vrv::Resources::SetPath([resourcePath cStringUsingEncoding:NSUTF8StringEncoding]);
-            if (!vrv::Resources::InitFonts()) {
-                NSAssert(false, @"Can't init fonts");
-            }
-        });
-    }
-    return self;
-}
-
 #pragma mark - rendering
 
 - (NSString *)renderFirstPageForURL:(NSURL *)url withOptions:(NSDictionary *)options {
     NSString * __block svg;
     dispatch_sync(renderQueue, ^{
+        NSBundle * verovioBundle = [BundleHelper bundleForModule];
+        NSString * resourcePath = [verovioBundle URLsForResourcesWithExtension:@"xml" subdirectory:@"data"].firstObject.URLByDeletingLastPathComponent.path;
+        vrv::Resources::SetPath([resourcePath cStringUsingEncoding:NSUTF8StringEncoding]);
+        if (!vrv::Resources::InitFonts()) {
+            NSAssert(false, @"Can't init fonts");
+        }
         vrv::Toolkit * toolkit = new vrv::Toolkit(false);
         NSError * error = nil;
         NSData * optionData = [NSJSONSerialization dataWithJSONObject:options options:0 error:&error];
